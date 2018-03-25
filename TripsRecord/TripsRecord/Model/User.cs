6f758d8f140cf243e71b39ced2b8db472918ed70
uricodeleon <uricodeleon@gmail.com>
2018-03-25 00:37:48 -0700

    Adding NotifyPropertyChanged

--- TripsRecord/TripsRecord/Model/User.cs
+++ TripsRecord/TripsRecord/Model/User.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TripsRecord.Model
 {
-    public class User
+    public class User :INotifyPropertyChanged
     {
         public string Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
 
         //create user
